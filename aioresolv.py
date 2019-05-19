07b4af3ae13ef4672f8fed199d5b177429f63b69
Darkerego <darkerego@users.noreply.github.com>
2019-05-19 16:50:41 -0400

    Update aioresolv.py

--- aioresolv.py
+++ aioresolv.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 # AioMassResolver ~ uvloop & aiodns - resolve bulk dns queries extremely quickly
-# xelectron@protonmail.com
 
 """
-@author: darkerego
-@time: 2019/5/16
+author: darkerego ~  2019/5/19 ~ xelectron@protonmail.com
 """
 import random
 import string
-from typing import Dict, Any
 import argparse
 import aiodns
 import asyncio
 import ipaddress
 import uvloop
+from time import process_time
+from colorama import init, Fore, Style
+
+init(autoreset=True)
 
 
 class AsyncResolver(object):
-    results: Dict[Any, Any]
+    # results: Dict[Any, Any]
 
     def __init__(self, num_workers=5, nameservers=['8.8.8.8', '8.8.4.4'], loop=None, qtype="A", debug=False):
         self.timeout = 0.1
-        # self.loop = uvloop.new_event_loop()  # use uvloop
         # self.loop = asyncio.get_event_loop()
         self.loop = uvloop.new_event_loop()  # use uvloop
         asyncio.set_event_loop(self.loop)
@@ -34,9 +34,7 @@ class AsyncResolver(object):
         self.num_workers = num_workers
         self.results = {}
 
-        """
-        Functions for later implementation
-        """
+        # TODO: Make this async so we can run a list of wildcard queries
 
     def wildcard_A(self, domain):
         genrandstr = lambda i: ''.join(random.choices(string.ascii_lowercase + string.digits, k=i))
@@ -45,155 +43,17 @@ class AsyncResolver(object):
         try:
             f = self.loop.run_until_complete(reqs)
             data = list(set(map(lambda x: x[0].host, f)))
-            print('[*] Found wildcard dns record:{data}'.format(data=data))
+            print(Fore.GREEN + '[*] Found wildcard dns records:')
             return data
         except:
-            print('[*] Not Found wildcard dns record')
-            return False
+            return Fore.RED + '[!] Found no wildcard dns records.'
 
-    def query_A(self, domain):
-        """query DNS A records.
-        ex: {'domain': 'www.google.com','type': 'A','data': [u'93.46.8.89']}}
-        """
-        f = self.query(domain, 'A')
-        result = self.loop.run_until_complete(f)
-        data = list(map(lambda x: x.host, result))
-        item = {"domain": domain, "type": "A", "data": data}
-        return item
-
-    def query_AAAA(self, domain):
-        """query DNS AAAA records.
-        ex: {'domain': 'www.google.com','type': 'AAAA','data': [u'2404:6800:4005:809::200e']}}
-        """
-        f = self.query(domain, 'A')
-        result = self.loop.run_until_complete(f)
-        data = list(map(lambda x: x.host, result))
-        item = {"domain": domain, "type": "AAAA", "data": data}
-        return item
-
-    def query_CNAME(self, domain):
-        """query DNS CNAME records.
-        ex: {'domain': 'www.baidu.com','type': 'CNAME','data': [u'www.a.shifen.com']}}
-        """
-        f = self.query(domain, 'CNAME')
-        result = self.loop.run_until_complete(f)
-        data = result.cname
-        item = {"domain": domain, "type": "CNAME", "data": data}
-        return item
-
-    def query_MX(self, domain):
-        """query DNS MX records.
-        ex: {'domain':'google.com', 'type': 'MX', 'data': ['aspmx.l.google.com.',
-                                                           'alt3.aspmx.l.google.com.',
-                                                           'alt4.aspmx.l.google.com.',
-                                                           'alt1.aspmx.l.google.com.',
-                                                           'alt2.aspmx.l.google.com.']}}
-        """
-        f = self.query(domain, 'MX')
-        result = self.loop.run_until_complete(f)
-        data = list(map(lambda x: x.host, result))
-        item = {"domain": domain, "type": "MX", "data": data}
-        return item
-
-    def query_NS(self, domain):
-        """query DNS NS records.
-        ex: {'domain': 'google.com','type':'NS','data': ['ns3.google.com.',
-                                                        'ns1.google.com.',
-                                                        'ns4.google.com.',
-                                                        'ns2.google.com.']}}
-        """
-        f = self.query(domain, 'NS')
-        result = self.loop.run_until_complete(f)
-        data = list(map(lambda x: x.host, result))
-        item = {"domain": domain, "type": "NS", "data": data}
-        return item
-
-    def query_SOA(self, domain):
-        """query DNS SOA records.
-        ex: {'domain': 'google.com', 'type': 'SOA', 'data': ['ns4.google.com.']}}
-        """
-        f = self.query(domain, 'SOA')
-        result = self.loop.run_until_complete(f)
-        data = result.nsname
-        item = {"domain": domain, "type": "SOA", "data": data}
-        return item
-
-    def query_SRV(self, domain):
-        """query DNS SRV records.,ex:
-        {'domain','xmpp-server._tcp.jabber.org', 'type': 'SRV', 'data': ['hermes2v6.jabber.org','hermes2.jabber.org']}
-        """
-        f = self.query(domain, 'SRV')
-        result = self.loop.run_until_complete(f)
-        data = list(map(lambda x: x.host, result))
-        item = {"domain": domain, "type": "SRV", "data": data}
-        return item
-
-    def query_TXT(self, domain):
-        """query DNS TXT records, normally, it includes [SPF] information.
-        ex: {'domain': 'yahoo.com', 'type': 'TXT', 'data': ['v=spf1 redirect=_spf.mail.yahoo.com']}}
-        """
-        f = self.query(domain, 'TXT')
-        result = self.loop.run_until_complete(f)
-        data = list(map(lambda x: x.text, result))
-        item = {"domain": domain, "type": "TXT", "data": data}
-        return item
-
-    def query_NAPTR(self, domain):
-        """query DNS NAPTR records, ex:
-        {'domain': 'sip2sip.info', 'type': 'NAPTR', 'data': ['_sip._udp.sip2sip.info',
-                                                            '_sip._tcp.sip2sip.info',
-                                                            '_sips._tcp.sip2sip.info']}}
-        """
-        f = self.query(domain, 'NAPTR')
-        result = self.loop.run_until_complete(f)
-        data = list(map(lambda x: x.replacement, result))
-        item = {"domain": domain, "type": "NAPTR", "data": data}
-        return item
-
-    def query_PTR(self, ip):
-        """query DNS PTR records, ex:
-        {'domain': 'google-public-dns-a.google.com', 'type': 'PTR', 'data': '8.8.8.8'}}
-        """
-        f = self.query(ipaddress.ip_address(ip).reverse_pointer, 'PTR')
-        result = self.loop.run_until_complete(f)
-        data = result.name
-        item = {"domain": data, "type": "PTR", "data": [ip]}
-        return item
-
-    """async def _query(self, domain, qtype):
-         self.domain = domain
-         self.qtype = qtype
-         # return await self.resolver.query(domain, type)
-         if domain is not None:
-            if qtype == 'wildcard_A':
-                return self.wildcard_A(domain)
-            elif qtype == 'A':
-                return self.query_A(domain)
-            elif qtype == 'AAAA':
-                return self.query_AAAA(domain)
-            elif qtype == 'CNAME':
-                return self.query_CNAME(domain)
-            elif qtype == 'MX':
-                return self.query_MX(domain)
-            elif qtype == 'NS':
-                return self.query_NS(domain)
-            elif qtype == "SOA":
-                return self.query_SOA(domain)
-            elif qtype == 'TXT':
-                return self.query_TXT(domain)
-            elif qtype == 'SRV':
-                return self.query_SRV(domain)
-            elif qtype == 'NAPTR':
-                return self.query_NAPTR(domain)
-            elif qtype == 'PTR':
-                return self.query_PTR(domain)
-            else:
-                return 'Error: Invalid dns record qtype'"""
 
     async def query(self, domain, qtype):
+
         return await self.resolver.query(domain, qtype)
 
-    def task_query(self, domains):
+    def task_query(self, domains, output):
 
         tasks = []
         q = asyncio.Queue()
@@ -202,18 +62,29 @@ class AsyncResolver(object):
             q.put_nowait(domain)
 
         for i in range(self.num_workers):
-            tasks.append(self.do_work(q))
+            tasks.append(self.do_work(q, output))
 
         self.loop.run_until_complete(asyncio.wait(tasks))
         return self.results
 
-    async def do_work(self, work_queue):
+    async def do_work(self, work_queue, output):
+
+        def writer(_data):
+            """
+             Logger function
+            :param _data: data to write to file
+            :return: -
+            """
+            with open(output, 'a') as ff:
+                ff.write(str(_data) + "\n")
+
         resolver = aiodns.DNSResolver(loop=self.loop, nameservers=self.nameservers, timeout=2, tries=1)
         # resolver = aiodns.DNSResolver(loop=loop)
         while not work_queue.empty():
-            domain = await work_queue.get()
+            domain: object = await work_queue.get()
             qtype = self.qtype
             debug = self.debug
+            item = None
             try:
                 if qtype == 'PTR':
                     res = await resolver.query(ipaddress.ip_address(domain).reverse_pointer, 'PTR')
@@ -223,32 +94,70 @@ class AsyncResolver(object):
             except aiodns.error.DNSError as e:
                 error_code: object = e.args[0]
                 if self.debug:
-                    print("[DEBUG]: error: " + str(e))
+                    print(Fore.RED + "[DEBUG]: error: " + str(e))
                 if error_code == aiodns.error.ARES_ECONNREFUSED:
-                    self.results[domain] = "CONNECTION_REFUSED"
+                    self.results = "CONNECTION_REFUSED"
                 elif error_code == aiodns.error.ARES_ENODATA:
-                    self.results[domain] = "NODATA"
+                    self.results = "NODATA"
                 elif error_code == aiodns.error.ARES_ENOTFOUND:
-                    self.results[domain] = "NXDOMAIN"
+                    self.results = "NXDOMAIN"
                 elif error_code == aiodns.error.ARES_EREFUSED:
-                    self.results[domain] = "REFUSED"
+                    self.results = "REFUSED"
                 elif error_code == aiodns.error.ARES_ESERVFAIL:
-                    self.results[domain] = "SERVFAIL"
+                    self.results = "SERVFAIL"
                 elif error_code == aiodns.error.ARES_ETIMEOUT:
-                    self.results[domain] = "TIMEOUT"
+                    self.results = "TIMEOUT"
                 else:
                     self.results[domain] = "UNKNOWN_STATUS"
 
             except Exception as e:
-                print(str(domain + ' error: ' + str(e)))
+                print(str(domain) + ' error: ' + str(e))
                 if debug:
-                    print("[debug]: raw :" + str(e))
+                    print(Fore.RED + "[debug]: raw :" + str(e))
             else:
-                self.results[domain] = res
-                res = str(res)
-                print(res)
+                self.results = res
+                if qtype == 'A':
+                    data = list(map(lambda x: x.host, res))
+                    item = {"domain": domain, "type": "A", "data": data}
+                elif qtype == 'AAAA':
+                    data = list(map(lambda x: x.host, res))
+                    item = {"domain": domain, "type": "AAAA", "data": data}
+                elif qtype == 'CNAME':
 
-            work_queue.task_done()
+                    data = res.cname
+                    item = {"domain": domain, "type": "CNAME", "data": data}
+                elif qtype == 'MX':
+
+                    data = list(map(lambda x: x.host, res))
+                    item = {"domain": domain, "type": "MX", "data": data}
+                    print(item)
+                elif qtype == 'NS':
+
+                    data = list(map(lambda x: x.host, res))
+                    item = {"domain": domain, "type": "NS", "data": data}
+                elif qtype == 'SOA':
+                    data = res.nsname
+                    item = {"domain": domain, "type": "SOA", "data": data}
+                elif qtype == 'TXT':
+                    data = list(map(lambda x: x.text, res))
+                    item = {"domain": domain, "type": "TXT", "data": data}
+                elif qtype == 'SRV':
+                    data = list(map(lambda x: x.host, res))
+                    item = {"domain": domain, "type": "SRV", "data": data}
+                elif qtype == 'NAPTR':
+                    data = list(map(lambda x: x.replacement, res))
+                    item = {"domain": domain, "type": "NAPTR", "data": data}
+                elif qtype == 'PTR':
+                    data = res.name
+                    item = {"domain": data, "type": "PTR", "data": [domain]}
+                else:
+                    # self.results[domain] = res
+                    raise ValueError('Invalid query type.')
+            finally:
+                if item is not None:
+                    print(item)
+                    writer(item)
+                work_queue.task_done()
 
 
 class Resolver:
@@ -277,7 +186,7 @@ class Resolver:
         self.swarm = None
 
     def test_domain_list_resolve_ns(self):
-        self.swarm.task_query(self.domains)
+        self.swarm.task_query(self.domains, self.output)
 
 
 def line_gen(list_file):
@@ -290,16 +199,23 @@ def line_gen(list_file):
 
 def get_args():
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--demo", dest='demo', action='store_true', default=False,
-                        help='Demo mode. Show me what you got.')
+    usage = "Uvloop & AIODns Powered Resolver. Resolve thousands of domains in seconds.\n"
+    usage += "example usage: $ python3 ./aioresolver.py --list domains.list --qtype 'A' "
+    parser = argparse.ArgumentParser(usage)
+
     parser.add_argument('-o', "--out", dest='output', type=str, default='aioresolver.log')
-    parser.add_argument('-i', '--json', dest='json_output', action='store_true')
-    parser.add_argument('-w', '--workers', dest='num_workers', type=int, help='Number of async workers', default=1000)
+    # parser.add_argument('-i', '--json', dest='json_output', action='store_true')
+    parser.add_argument('-w', '--workers', dest='num_workers', type=int, help='Number of async workers to use.'
+                                                                              'Increase this for longer lists. '
+                                                                              'Default: 1000', default=1000)
     parser.add_argument('-l', '--list', dest='list_file', type=str, help='list of ips/domains to resolves')
     parser.add_argument('-d', '--domain', dest='single_query', type=str, help='Resolve a single domain.')
     parser.add_argument('-q', '--qtype', dest='qtype', help='DNS Query type to perform. Valid qtypes are:'
-                                              'A, AAAA, CNAME, NS, MX, SOA, TXT, PTR', type=str, default='A')
+                                              'A, AAAA, CNAME, NS, MX, SOA, TXT, PTR', type=str, default='A',
+                        choices=['A', 'AAAA', 'CNAME', 'NS', 'MX', 'SOA', 'TXT', 'PTR'])
+    parser.add_argument("-W", "--wildcard_A", dest='wildcard', type=str, default=False,
+                        help='Run a wildcard_A query against a single domain. Example: '
+                             './aiomassresolver.py -W taobao.com')
     args = parser.parse_args()
     return args
 
@@ -308,34 +224,32 @@ def main():
     args = get_args()
 
     def run(domains_ips, qtype, _output, _json_output, _limit):
-        _output = 'resolver.log'
         _json_output = False
         resolve = Resolver(domains_ips, qtype, _output, _json_output, _limit)
         resolve.test_domain_list_resolve_ns()
-
-    if args.demo:
-        demo = AsyncResolver()
-        print(demo.wildcard_A('taobao.com'))
-        print(demo.query_A('google.com'))
-        print(demo.query_AAAA('google.com'))
-        print(demo.query_CNAME('www.baidu.com'))
-        print(demo.query_NS('google.com'))
-        print(demo.query_MX('google.com'))
-        print(demo.query_SOA('google.com'))
-        print(demo.query_TXT('google.com'))
-        print(demo.query_PTR('8.8.8.8'))
+    if args.output:
+        output = args.output
+    if args.wildcard:
+        domain = args.wildcard
+        resolver = AsyncResolver()
+        print(resolver.wildcard_A(domain))
     else:
         limit = args.num_workers
-        output = args.output
-        json_output = args.json_output
+        # output = args.output
+        json_output = True  # TODO: remove this redundant argument
         _qtype = args.qtype
         if args.list_file:
             list_file = args.list_file
-            ips = line_gen(list_file)
-            _ips = []
-            for i in ips:
-                _ips.append(i)
-            run(_ips, _qtype, output, json_output, limit)
+            print(Fore.YELLOW + 'Reading from file %s' % list_file)
+            queries = line_gen(list_file)
+            _queries = []
+            for i in queries:
+                _queries.append(i)
+            t = process_time()
+            run(_queries, _qtype, output, json_output, limit)
+            elapsed_time = process_time() - t
+            print(Fore.YELLOW + "--------------------------------------------------------------------------")
+            print("Finished! Process time: " + str(elapsed_time))
 
         else:
             limit = 1
@@ -344,7 +258,7 @@ def main():
                 _domain.append(args.single_query)
                 run(_domain, _qtype, output, json_output, limit)
             else:
-                print('Specify either a list or  a query!')
+                print(Fore.RED + '[Error]: Specify either a list or  a query! Run --help for usage.')
                 exit(1)
 
 
